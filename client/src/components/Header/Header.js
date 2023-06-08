ef786f809fa2f4141ea627bba9b5c933bffa4dfe
ntr0pie <tripathi.shreeshv@gmail.com>
2023-06-08 16:22:01 +0530

    Add component placeholders

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+export default function Header({}) {
+  return (
+    <div className='header'>
+      <Button>Login</Button>
+      <Button>Sign Up</Button>
+    </div>
+  )
+}
+
