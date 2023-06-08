1b2998ed3a92a5885b453ea6002daafae444e083
ntr0pie <tripathi.shreeshv@gmail.com>
2023-06-08 18:38:04 +0530

    Add form submit logic

--- client/src/components/Authentication/Authentication.js
+++ client/src/components/Authentication/Authentication.js
@@ -1,9 +1,25 @@
 import React, { useState } from 'react'
 import Button from '@mui/material/Button';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Authentication({isLoggedInState, authTypeState}) {
-    function handleFormSubmit(){
-        
+    function handleFormSubmit(event){
+        event.preventDefault();
+        const userData = {email, password};
+        // const authUrl = '/auth' + authTypeState.state === 'login' ? '/login' : '/signup'
+        const authUrl = authTypeState.state === 'login' ? '/login' : '/signup'
+        try{
+            const response = await axios.post(authUrl, userData);
+            console.log(response.data);
+            isLoggedInState.setter(true);
+            const navigate = useNavigate();
+            navigate('/');
+        }
+        catch(erorr){
+            console.log(error);
+        }
     }
 
     const [email, setEmail] = useState('');
@@ -12,7 +28,11 @@ export default function Authentication({isLoggedInState, authTypeState}) {
   return (
     <div>
         <h1>{authTypeState.state === 'login' ? 'Login' : 'Sign Up'}</h1>
+
+        {/* Login/Sign up Form */}
         <form onSubmit={handleFormSubmit}>
+
+            {/* Email input */}
             <input 
                 type='email'
                 placeholder='Email'
@@ -20,6 +40,8 @@ export default function Authentication({isLoggedInState, authTypeState}) {
                 onChange={(event) => setEmail(event.target.value)}
                 required
             />
+            
+            {/* Password input */}
             <input
                 type='password'
                 placeholder='Password'
@@ -27,6 +49,7 @@ export default function Authentication({isLoggedInState, authTypeState}) {
                 onChange={(event) => setPassword(event.target.value)}
                 required
             />
+
             <Button type='submit'>
                 {authTypeState.state === 'login' ? 'Login' : 'Sign Up'}
             </Button>
